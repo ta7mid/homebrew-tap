4fd6cccd53adfdb729857f0f5aaa5cc33db468bc
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2025-05-24 15:51:08 +0600

    refactor(formula): plan9port: improve recipe and prose

--- Formula/plan9port.rb
+++ Formula/plan9port.rb
@@ -12,40 +12,48 @@ class Plan9port < Formula
   depends_on "grep" => :build
 
   def install
-    # prepare
+    # clean up
+    rm_rf ".hg/"
+    rm_f Dir["**/.cvsignore"]
+    rm_f [".hgignore", ".hgtags", "CONTRIBUTING.md", "CONTRIBUTORS"]
     on_linux do
       rm_rf "mac/"
     end
-    # https://gitlab.archlinux.org/archlinux/packaging/packages/plan9port/-/blob/7045c67c217a4b27af666ac48fe9f4997b6c18cc/PKGBUILD#L47
+
+    # prepare
+    root = prefix/"plan9"
     grep = Formula["grep"].opt_bin/"ggrep"
     xargs = Formula["findutils"].opt_bin/"gxargs"
     sed = Formula["gnu-sed"].opt_bin/"gsed"
-    system("#{grep} --null -l -r /usr/local/plan9 | #{xargs} --null #{sed} -i 's!/usr/local/plan9!#{libexec.to_s}!g'")
-    libexec.install Dir["*"]
+    # https://gitlab.archlinux.org/archlinux/packaging/packages/plan9port/-/blob/7045c67c217a4b27af666ac48fe9f4997b6c18cc/PKGBUILD#L47
+    system("#{grep} --null -l -r /usr/local/plan9 | #{xargs} --null #{sed} -i 's!/usr/local/plan9!#{root.to_s}!g'")
+    root.install Dir["*"]
 
-    chdir libexec do
+    chdir root do
       # build
-      system "./INSTALL", "-r", libexec.to_s
+      system "./INSTALL", "-r", root.to_s
 
       # install
-      bin.install_symlink libexec/"bin/9"
+      bin.install_symlink root/"bin/9"
+      cp "bin/9", bin/"plan9"
 
       # clean up
-      rm_rf ".hg/"
-      rm_f [".hgignore", ".hgtags", "CONTRIBUTING.md", "CONTRIBUTORS", "INSTALL", "Makefile", "config", "configure", "install.log", "install.sum", "install.txt"]
-      rm_f Dir["**/.cvsignore"]
+      rm_f ["INSTALL", "Makefile", "config", "configure", "install.log", "install.sum", "install.txt"]
+      prefix.install "CHANGES", "LICENSE", "README.md"
     end
   end
 
   def caveats
     <<~EOS
       In order not to collide with system binaries, the Plan 9 binaries have
-      been installed to #{opt_libexec}/bin.
+      been installed to #{opt_prefix}/plan9/bin.
 
-      To run the Plan 9 version of a command simply call it through the command
-      "9", which has been installed into the Homebrew prefix bin.  For example,
-      to run Plan 9's ls run:
+      To run the Plan 9 version of a command, simply call it through either
+      `9` or `plan9`, which have been installed into the Homebrew prefix bin.
+      For example, to run Plan 9's `ls`, enter
           # 9 ls
+      or
+          # plan9 ls
     EOS
   end
 

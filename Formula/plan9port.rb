17e1ac1cb4225b9bd1d0e754fe3e896aa3208062
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2025-05-25 12:51:49 +0600

    refactor(formula): plan9port: improve recipe and style, pass brew audits
    Copy .rubocop.yml from Homebrew/Homebrew-core

--- Formula/plan9port.rb
+++ Formula/plan9port.rb
@@ -1,74 +1,68 @@
 class Plan9port < Formula
   desc "Port of many Plan 9 libraries and programs to Unix"
   homepage "https://9fans.github.io/plan9port/"
-  url "https://github.com/9fans/plan9port/archive/df9b195e.tar.gz"
+  url "https://github.com/9fans/plan9port/archive/refs/heads/master.tar.gz"
   version "df9b195e"
-  sha256 "920232bc57c41b019e2254d67cbb332fe4dd5a5e90334a9174e8d0b51c4f0624"
-  head "https://github.com/9fans/plan9port.git"
+  sha256 "7c9eddf9506149e8683dbba7d13d08e2defa443ff402f1b591e813d3920cb2bb"
   license "MIT"
+  head "https://github.com/9fans/plan9port.git", branch: "master"
 
   def install
     # remove cruft
-    rm_rf ".hg/"
-    rm_f Dir["**/.cvsignore"]
-    rm_f [".hgignore", ".hgtags", "CONTRIBUTING.md", "CONTRIBUTORS"]
-    on_linux do
-      rm_rf "mac/"
-    end
+    rm ["CONTRIBUTING.md", "CONTRIBUTORS"]
+    rm_r "mac/" if OS.linux?
 
     # prepare
     # https://gitlab.archlinux.org/archlinux/packaging/packages/plan9port/-/blob/7045c67c217a4b27af666ac48fe9f4997b6c18cc/PKGBUILD#L47
     Dir["**/*"]
-      .select { |path|
-        File.file?(path) and File.foreach(path).any?{ |line| line.include? "/usr/local/plan9" }
-      }.each { |file| inreplace file, "/usr/local/plan9", libexec.to_s }
+      .select { |path| File.file?(path) }
+      .select { |file| File.foreach(file).any? { |line| line.include? "/usr/local/plan9" } }
+      .each { |file| inreplace file, "/usr/local/plan9", libexec.to_s }
     libexec.install Dir["*"]
 
-    # build
     chdir libexec do
+      # build
       system "./INSTALL", "-r", libexec.to_s
-    end
 
-    # install
-    (libexec/"bin").children.each do |cmd|
-      bin.install_symlink cmd => "plan9-#{File.basename cmd}"
-    end
-    File.rename bin/"plan9-9", bin/"9"
-    (libexec/"man").glob("man*/*").each do |page|
-      dir = File.basename(File.dirname page)
-      linked_name = File.basename page
-      unless linked_name == "INDEX" or linked_name == "index.html"
-        linked_name = "plan9-#{linked_name}"
+      # install
+      Dir["bin/*"].each do |cmd|
+        bin.install_symlink cmd => "plan9-#{File.basename cmd}"
       end
-      (man/dir).install_symlink page => linked_name
-    end
-    prefix.install libexec/"CHANGES", libexec/"LICENSE", libexec/"README.md"
+      mv bin/"plan9-9", bin/"9"
+      Dir["man/man*/*"].each do |page|
+        basename = File.basename page
+        index_basenames = ["INDEX", "index.html"]
+        dir = File.basename(File.dirname(page))
+        (man/dir).install_symlink page => "plan9-#{basename}" unless index_basenames.include?(basename)
+      end
+      prefix.install ["CHANGES", "LICENSE", "README.md"]
 
-    # clean up
-    rm_f [
-      libexec/"INSTALL",
-      libexec/"Makefile",
-      libexec/"config",
-      libexec/"configure",
-      libexec/"install.log",
-      libexec/"install.sum",
-      libexec/"install.txt",
-    ]
+      # clean up
+      rm %w[
+        INSTALL
+        Makefile
+        config
+        configure
+        install.log
+        install.sum
+        install.txt
+      ]
+    end
   end
 
   def caveats
     <<~EOS
-      In order not to collide with system binaries, the Plan 9 binaries
-      have not been installed to the Homebrew prefix directory as is.
-      Instead, they have been installed into #{Formatter.url opt_libexec/"bin"}
+      In order not to collide with system binaries, the Plan 9 binaries have not
+      been installed to the Homebrew prefix directory as is.  Instead, they have
+      been installed into #{Formatter.url opt_libexec/"bin"}
       and symlinked into the Homebrew prefix bin with `plan9-` prepended to
-      their names.  Likewise, the Plan 9 man pages have also been symlinked
-      into the Homebrew prefix with `plan9-`-prefixed names.
+      their names.  Likewise, the Plan 9 man pages have also been symlinked into
+      the Homebrew prefix with `plan9-`-prefixed names.
 
       Several of the installed tools expect other Plan 9 binaries to be
-      available in PATH.  The `9` command can be used to adjust the PATH on
-      the fly and use the original names of the binaries.  Hence, to run the
-      Plan 9 version of a command, simply prefix it with `9 `, like so:
+      available in PATH.  The `9` command can be used to adjust the PATH on the
+      fly and use the original names of the binaries.  Hence, to run the Plan 9
+      version of a command, simply prefix it with `9 `, like so:
         # 9 ls
 
       If you want the unprefixed versions always available in your PATH, add
@@ -81,7 +75,7 @@ class Plan9port < Formula
   end
 
   test do
-    (testpath/"test.c").write <<~EOS
+    (testpath/"test.c").write <<~C
       #include <u.h>
       #include <libc.h>
       #include <stdio.h>
@@ -90,7 +84,7 @@ class Plan9port < Formula
       {
         return printf("Hello World\\n");
       }
-    EOS
+    C
     system bin/"9", "9c", "test.c"
     system bin/"9", "9l", "-o", "test", "test.o"
     assert_equal "Hello World\n", shell_output("./test", 1)

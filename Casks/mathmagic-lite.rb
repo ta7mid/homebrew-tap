99fba9cf4e3a0494f479062fb65060b5180edf3f
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-03-28 21:24:30 +0600

    mathmagic-lite: update version to 10.41; various fixes and improvements

--- Casks/mathmagic-lite.rb
+++ Casks/mathmagic-lite.rb
@@ -1,20 +1,35 @@
 cask "mathmagic-lite" do
-  version "10.15"
-  sha256 "8034861b27c13bfeffdfb58bf9e573203965a37711490f25096b108350623e89"
+  version "10.41"
+  sha256 :no_check
 
   url "http://www.mathmagic.com/download/installers/MathMagicLite.64-bit.zip"
   name "MathMagic Lite"
-  desc "Multi-purpose editor and converter for equations and scientific symbols"
-  homepage "http://www.mathmagic.com"
+  desc "Editor and format converter for mathematical expressions"
+  homepage "http://www.mathmagic.com/"
 
   livecheck do
     url "http://www.mathmagic.com/download/download.html"
-    regex /<a href="http:\/\/www.mathmagic.com\/download\/installers\/MathMagicLite.64-bit.zip">MathMagic Lite v([1-9]+[0-9]*[._-][0-9]+) for Mac<\/a>/i
+    regex(/<a href="[^">]*?MathMagicLite.64-bit.zip">MathMagic Lite v?(\d+(?:\.\d+)+) for Mac[^\n]+64-bit/i)
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
   suite "MathMagic Lite 64-bit"
 
-  zap trash: ""
+  zap trash: [
+        "~/Documents/MathMagic Lite User Data5",
+        "~/Library/Application Scripts/com.InfoLogic.MathMagic-Lite",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.infologic.mathmagic-lite.sfl*",
+        "~/Library/Caches/com.InfoLogic.MathMagic-Lite",
+        "~/Library/Containers/com.InfoLogic.MathMagic-Lite",
+        "~/Library/HTTPStorages/com.InfoLogic.MathMagic-Lite",
+        "~/Library/Preferences/com.InfoLogic.MathMagic_Lite.Defaults.plist",
+        "~/Library/Preferences/MathMagic Lite #{version.major}.1.pref.plist",
+        "~/Library/Preferences/MathMagic Lite #{version.major}.pref.plist",
+      ],
+      rmdir: "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments"
+
+  caveats do
+    requires_rosetta
+  end
 end

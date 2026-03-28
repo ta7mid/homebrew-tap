c58a051571cbdf8de13fac3754ca347c542bb73f
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-03-28 21:02:05 +0600

    cask plan9port: use Git URL and simplify preflight

--- Casks/plan9port.rb
+++ Casks/plan9port.rb
@@ -2,8 +2,10 @@ cask "plan9port" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/9fans/plan9port/archive/refs/heads/master.tar.gz",
-      verified: "github.com/9fans/plan9port/"
+  url "https://github.com/9fans/plan9port.git",
+      verified:  "github.com/9fans/plan9port",
+      branch:    "master",
+      only_path: "mac"
   name "plan9port macOS apps"
   desc "App bundles for 9term(1) and plumb(1) from plan9port"
   homepage "https://9fans.github.io/plan9port/"
@@ -14,19 +16,13 @@ cask "plan9port" do
   app "Plumb.app"
 
   preflight do
-    src = staged_path/"plan9port-master"
-
-    (src/"mac").glob("*.app").each do |app|
+    Dir["*.app"].each do |app|
       launcher = app/"Contents/MacOS"/app.basename(".app").to_s.downcase
       contents = launcher.read
 
       contents.sub! "/usr/local/plan9", Formula["plan9port"].opt_libexec
       launcher.write contents
       launcher.chmod 0755
-
-      FileUtils.mv app, staged_path
     end
-
-    FileUtils.rm_r src
   end
 end

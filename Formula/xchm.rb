ca804d853225a7f92efd055b995c9ed75b201685
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-04-03 09:42:14 +0600

    xchm: fix and update

--- Formula/xchm.rb
+++ Formula/xchm.rb
@@ -1,16 +1,33 @@
 class Xchm < Formula
-  desc "CHM file viewer"
+  desc "GUI front-end to chmlib"
   homepage "https://github.com/rzvncj/xCHM"
-  url "https://github.com/rzvncj/xCHM/releases/download/1.36/xchm-1.36.tar.gz"
-  sha256 "291411f51fe702f7f60a2904757183cfb7b7a0852e008bd71dcbfbe573ea9f43"
-  license "GPL-2.0"
+  url "https://github.com/rzvncj/xCHM/archive/refs/tags/1.38.tar.gz"
+  sha256 "f6db981faa426b29d432e0fcfc83fc5b6192972cd02fe9412b0348f425e9b60d"
+  license "GPL-2.0-or-later"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "chmlib"
-  depends_on "wxwidgets"
+  depends_on "gettext" => :no_linkage
+  depends_on "libiconv" => :no_linkage
+  depends_on "wxwidgets@3.2"
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--disable-silent-rules", *std_configure_args
-    system "make"
+    args = %W[
+      --disable-silent-rules
+      --enable-optimize
+      --with-libiconv-prefix=#{Formula["libiconv"].prefix}
+      --with-libintl-prefix=#{Formula["gettext"].prefix}
+      --with-wx-prefix=#{Formula["wxwidgets@3.2"].prefix}
+      --with-wx-config=#{Formula["wxwidgets@3.2"].bin/"wx-config-3.2"}
+    ]
+
+    system "autoreconf", "--force", "--install", "--verbose"
+    system "./configure", *args, *std_configure_args
     system "make", "install"
+  end
+
+  test do
+    system bin/"xchm", "--help"
   end
 end
